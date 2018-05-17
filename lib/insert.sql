85a32bd566183484538f037a8124c48ed4fa9134
Fiona Lapham <fionadlapham@gmail.com>
2018-05-17 11:19:14 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,2 @@
-INSERT INTO (name, age, gender, color, temperment, alive) VALUES 
-(Mr. Chocolate, 5, M, brown, funny, 1)
+INSERT INTO (name, age, gender, color, temperment, alive) VALUES
+(Mr. Chocolate, 5, M, brown, funny, 1);
